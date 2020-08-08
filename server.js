25f8fab72cefa702e21b25e5ae68d616d9491445
ThibsVW <thibault.vanweydeveld@hotmail.com>
2020-08-08 19:35:51 +0800

    json updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+const { Sequelize } = require('sequelize');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
